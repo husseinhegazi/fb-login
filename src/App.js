a59728c9cd2822fea7ecd80cd8fe4494d4e67e13
husseinhegazi <108274928+husseinhegazi@users.noreply.github.com>
2023-01-16 17:48:49 +0200

    login fb

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useState } from 'react'
+import './App.css'
+import {
+  LoginSocialGoogle,
+  LoginSocialAmazon,
+  LoginSocialFacebook,
+  LoginSocialGithub,
+  LoginSocialInstagram,
+  LoginSocialLinkedin,
+  LoginSocialMicrosoft,
+  LoginSocialPinterest,
+  LoginSocialTwitter,
+  LoginSocialApple,
+  LoginSocialTiktok,
+} from 'reactjs-social-login'
+import {
+  FacebookLoginButton,
+  GoogleLoginButton,
+  GithubLoginButton,
+  AmazonLoginButton,
+  InstagramLoginButton,
+  LinkedInLoginButton,
+  MicrosoftLoginButton,
+  TwitterLoginButton,
+  AppleLoginButton,
+} from 'react-social-login-buttons'
 
 function App() {
+  const [provider, setProvider] = useState('')
+  const [profile, setProfile] = useState(null)
+
+  const onLoginStart = useCallback(() => {
+    alert('login start')
+  }, [])
+
+  const onLogoutSuccess = useCallback(() => {
+    setProfile(null)
+    setProvider('')
+    alert('logout success')
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div className={`App ${provider && profile ? 'hide' : ''}`}>
+        <h1 className="title">ReactJS Social Login</h1>
+        <LoginSocialFacebook
+          isOnlyGetToken
+          appId={process.env.REACT_APP_FB_APP_ID || '2432691326883003'}
+          onLoginStart={onLoginStart}
+          onResolve={({ provider, data }) => {
+            setProvider(provider)
+            setProfile(data)
+            console.log("data=>",data)
+            console.log("provider=>",provider)
+          }}
+          onReject={(err) => {
+            console.log("error=>",err)
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <FacebookLoginButton />
+        </LoginSocialFacebook>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
